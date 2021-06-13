12057fb4eb36eeb19fbc3300383a0cc5a0f2eef6
Gian Millare <gianmillaregsm@gmail.com>
2021-06-13 13:44:19 -0700

    imported all libraries in streamlit app. created the load_data function and started overview section

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ import numpy as np
 import pydeck as pdk
 import matplotlib.pyplot as plt
 
+DATA_URL = ("resources/cfwb.csv")
+
+st.title("Does Money equal happiness?")
+st.markdown("### An overview data analysis on life satisfaction and financial well-being!")
+
+@st.cache(persist=True)
+def load_data():
+    original_data = pd.read_csv(DATA_URL)
+    data = original_data[['SWB_1', 'FWBscore', 'ENDSMEET', 'FSscore', 'LMscore']]
+    return data
+
+data = load_data()
+
+# Overview
